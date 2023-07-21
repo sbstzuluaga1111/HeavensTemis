b299644d49a0ae222eb7ba7b32e4fc4225df95b2
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-07-21 12:36:37 -0500

    Databases

---
+++ Backend/Heavens.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE informacion_geografica;
+USE informacion_geografica;
+
+CREATE TABLE Departamentos (
+    id_departamento INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_departamento VARCHAR(100),
+);
+
+CREATE TABLE Municipios (
+    id_municipio INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_municipio VARCHAR(100),
+    id_departamento INT,
+    Foreign Key (id_departamento) REFERENCES Departamentos(id_departamento)
+);
+
+CREATE TABLE Comunas (
+    id_comuna INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_comuna VARCHAR(100),
+    id_municipio INT,
+    Foreign Key (id_municipio) REFERENCES Municipios(id_municipio)
+);
+
+CREATE TABLE Barrios (
+    id_barrio INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_barrio VARCHAR(100),
+    id_comuna INT,
+    Foreign Key (id_comuna) REFERENCES Barrios(id_comuna)
+);
+
+CREATE TABLE Creyente (
+    id_creyente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_creyente VARCHAR(100),
+    id_barrio INT,
+    Foreign Key (id_barrio) REFERENCES Creyente(id_barrio)
+);
+
